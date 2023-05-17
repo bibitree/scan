bce8f99115cf2a6f39df95b0a5e8e373a26ee4ce
a821808553 <821808553@qq.com>
2023-05-17 18:05:23 +0800

    commit message

--- sniffer/event.go
+++ sniffer/event.go
@@ -9,41 +9,53 @@ import (
 )
 
 type Event struct {
-	Address      common.Address         `json:"address"`
-	ContractName string                 `json:"contractName"`
-	ChainID      *big.Int               `json:"chainID"`
-	Data         map[string]interface{} `json:"data"`
-	BlockHash    common.Hash            `json:"blockHash"`
-	BlockNumber  string                 `json:"blockNumber"`
-	Name         string                 `json:"name"`
-	TxHash       common.Hash            `json:"txHash"`
-	TxIndex      string                 `json:"txIndex"`
-	Gas          uint64                 `json:"gas"`
-	GasPrice     *big.Int               `json:"gasPrice"`
-	GasTipCap    *big.Int               `json:"gasTipCap"`
-	GasFeeCap    *big.Int               `json:"gasFeeCap"`
-	Value        string                 `json:"value"`
-	Nonce        uint64                 `json:"nonce"`
-	To           common.Address         `json:"to"`
+	Address          common.Address         `json:"address"`
+	ContractName     string                 `json:"contractName"`
+	ChainID          *big.Int               `json:"chainID"`
+	Data             map[string]interface{} `json:"data"`
+	BlockHash        common.Hash            `json:"blockHash"`
+	BlockNumber      string                 `json:"blockNumber"`
+	Name             string                 `json:"name"`
+	TxHash           common.Hash            `json:"txHash"`
+	TxIndex          string                 `json:"txIndex"`
+	Gas              uint64                 `json:"gas"`
+	GasPrice         *big.Int               `json:"gasPrice"`
+	GasTipCap        *big.Int               `json:"gasTipCap"`
+	GasFeeCap        *big.Int               `json:"gasFeeCap"`
+	Value            string                 `json:"value"`
+	Nonce            uint64                 `json:"nonce"`
+	To               common.Address         `json:"to"`
+	Status           bool                   `json:"status"`
+	Timestamp        uint64                 `json:"timestamp"`
+	MinerAddress     string                 `json:"minerAddress"`
+	Size             common.StorageSize     `json:"size"`
+	BlockReward      string                 `json:"blockReward"`
+	AverageGasTipCap string                 `json:"averageGasTipCap"`
 }
 
 type Event2 struct {
-	Address      common.Address `json:"address"`
-	ContractName string         `json:"contractName"`
-	ChainID      *big.Int       `json:"chainID"`
-	Data         []byte         `json:"data"`
-	BlockHash    common.Hash    `json:"blockHash"`
-	BlockNumber  string         `json:"blockNumber"`
-	Name         string         `json:"name"`
-	TxHash       common.Hash    `json:"txHash"`
-	TxIndex      string         `json:"txIndex"`
-	Gas          uint64         `json:"gas"`
-	GasPrice     *big.Int       `json:"gasPrice"`
-	GasTipCap    *big.Int       `json:"gasTipCap"`
-	GasFeeCap    *big.Int       `json:"gasFeeCap"`
-	Value        string         `json:"value"`
-	Nonce        uint64         `json:"nonce"`
-	To           common.Address `json:"to"`
+	Address          common.Address     `json:"address"`
+	ContractName     string             `json:"contractName"`
+	ChainID          *big.Int           `json:"chainID"`
+	Data             []byte             `json:"data"`
+	BlockHash        common.Hash        `json:"blockHash"`
+	BlockNumber      string             `json:"blockNumber"`
+	Name             string             `json:"name"`
+	TxHash           common.Hash        `json:"txHash"`
+	TxIndex          string             `json:"txIndex"`
+	Gas              uint64             `json:"gas"`
+	GasPrice         *big.Int           `json:"gasPrice"`
+	GasTipCap        *big.Int           `json:"gasTipCap"`
+	GasFeeCap        *big.Int           `json:"gasFeeCap"`
+	Value            string             `json:"value"`
+	Nonce            uint64             `json:"nonce"`
+	To               common.Address     `json:"to"`
+	Status           bool               `json:"status"`
+	Timestamp        uint64             `json:"timestamp"`
+	MinerAddress     string             `json:"minerAddress"`
+	Size             common.StorageSize `json:"size"`
+	BlockReward      string             `json:"blockReward"`
+	AverageGasTipCap string             `json:"averageGasTipCap"`
 }
 
 func (event *Event) IsEmpty() bool {
